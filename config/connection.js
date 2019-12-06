a34df161cef6882a52009bc8fc2d9ec957c6d66a
EINNNA <annie_t_@hotmail.com>
2019-12-06 20:46:39 +1100

    adding jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,19 @@
 var mysql = require("mysql");
 
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL) ;
+} else {
+        connection = mysql.createConnection({
+            host: "localhost",
+            user: "root",
+            password: "asdfgh",
+            database: "burgers_db"
+        });
+};
+
+connection.connect();
+module.exports = connection;
+
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
